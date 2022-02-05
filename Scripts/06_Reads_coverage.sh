da6c50bac13d07ec26b53dd926949bcbf8790524
Virginie Ricci <ninet93@hotmail.com>
2022-02-05 15:33:50 +0100

    .sorted

--- Scripts/06_Reads_coverage.sh
+++ Scripts/06_Reads_coverage.sh
@@ -20,7 +20,7 @@ ID=$(sed -n ${SLURM_ARRAY_TASK_ID}p Data/Dataset.txt | cut -f4) # To run as an a
 module purge
 module load BEDTools/2.27.1-foss-2018b
 
-bedtools genomecov -ibam ${BWA}_${ID}.bam | grep "^genome" | head 100 > ${BWA}_${ID}_genomecov.txt
+bedtools genomecov -ibam ${BWA}_${ID}.sorted.bam | grep "^genome" | head 100 > ${BWA}_${ID}_genomecov.txt
 	
 
 
@@ -28,13 +28,13 @@ bedtools genomecov -ibam ${BWA}_${ID}.bam | grep "^genome" | head 100 > ${BWA}_$
 module purge
 module load SAMtools/1.7-goolf-1.7.20
 
-samtools depth -aa -r "NC_031984.2:14827848-14828912" ${BWA}_${ID}_RH1.bam > ${BWA}_${ID}_RH1_CDS.bam.depth
+samtools depth -aa -r "NC_031984.2:14827848-14828912" ${BWA}_${ID}_RH1.sorted.bam > ${BWA}_${ID}_RH1_CDS.sorted.bam.depth
 
-samtools depth -aa -r "NC_031984.2:14387849-14388209" ${BWA}_${ID}_exoRH1.bam > ${BWA}_${ID}_exoRH1_CDS.bam.depth
-samtools depth -aa -r "NC_031984.2:14388439-14388607" ${BWA}_${ID}_exoRH1.bam >> ${BWA}_${ID}_exoRH1_CDS.bam.depth
-samtools depth -aa -r "NC_031984.2:14388789-14388954" ${BWA}_${ID}_exoRH1.bam >> ${BWA}_${ID}_exoRH1_CDS.bam.depth
-samtools depth -aa -r "NC_031984.2:14390612-14390851" ${BWA}_${ID}_exoRH1.bam >> ${BWA}_${ID}_exoRH1_CDS.bam.depth
-samtools depth -aa -r "NC_031984.2:14391687-14391806" ${BWA}_${ID}_exoRH1.bam >> ${BWA}_${ID}_exoRH1_CDS.bam.depth
+samtools depth -aa -r "NC_031984.2:14387849-14388209" ${BWA}_${ID}_exoRH1.sorted.bam > ${BWA}_${ID}_exoRH1_CDS.sorted.bam.depth
+samtools depth -aa -r "NC_031984.2:14388439-14388607" ${BWA}_${ID}_exoRH1.sorted.bam >> ${BWA}_${ID}_exoRH1_CDS.sorted.bam.depth
+samtools depth -aa -r "NC_031984.2:14388789-14388954" ${BWA}_${ID}_exoRH1.sorted.bam >> ${BWA}_${ID}_exoRH1_CDS.sorted.bam.depth
+samtools depth -aa -r "NC_031984.2:14390612-14390851" ${BWA}_${ID}_exoRH1.sorted.bam >> ${BWA}_${ID}_exoRH1_CDS.sorted.bam.depth
+samtools depth -aa -r "NC_031984.2:14391687-14391806" ${BWA}_${ID}_exoRH1.sorted.bam >> ${BWA}_${ID}_exoRH1_CDS.sorted.bam.depth
 
 
 

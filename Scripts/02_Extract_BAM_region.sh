dcfab0d6979df23a5d94a9bcd82e9f5352c07447
Virginie Ricci <ninet93@hotmail.com>
2022-02-05 15:31:22 +0100

    Update

--- Scripts/02_Extract_BAM_region.sh
+++ Scripts/02_Extract_BAM_region.sh
@@ -20,7 +20,10 @@ ID=$(sed -n ${SLURM_ARRAY_TASK_ID}p Data/Dataset.txt | cut -f4) # To run as an a
 
 # Extract mRNA BAM region
 samtools view -Sb ${BWA}_${ID}.sorted.bam "NC_031984.2:14827754-14829360" > ${BWA}_${ID}_RH1.sorted.bam
+samtools index ${BWA}_${ID}_RH1.sorted.bam
+
 samtools view -Sb ${BWA}_${ID}.sorted.bam "NC_031984.2:14387282-14393197" > ${BWA}_${ID}_exoRH1.sorted.bam
+samtools index ${BWA}_${ID}_exoRH1.sorted.bam
 
 # Next steps on Geneious (see Materials and Methods)
 
